d44c5ac0267ddc5f67519757b1a58ff1442fdc6c
zherunliu <1437257281@qq.com>
2026-02-05 17:39:29 +0800

    feat: effect (return runner when call)

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -16,4 +16,16 @@ describe("effect", () => {
     user.age++;
     expect(nextAge).toBe(12);
   });
+  it("return runner when call effect", () => {
+    // effect(fn) -> function(runner) -> fn -> return
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+    expect(foo).toBe(11);
+    const res = runner();
+    expect(foo).toBe(12);
+    expect(res).toBe("foo");
+  });
 });
