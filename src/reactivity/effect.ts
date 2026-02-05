4cd374b0818e75bf16a4fb9fed4764dcfe5e1c12
zherunliu <1437257281@qq.com>
2026-02-05 18:17:41 +0800

    feat: effect (execute schedule when updating)

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,9 @@
 class ReactiveEffect {
   private _fn;
-  constructor(fn) {
+  constructor(
+    fn,
+    public scheduler?,
+  ) {
     this._fn = fn;
   }
   run() {
@@ -28,13 +31,17 @@ export function trigger(target, key) {
   const depsMap = targetMap.get(target);
   const dep = depsMap.get(key);
   for (const effect of dep) {
-    effect.run();
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   }
 }
 
 let activeEffect;
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler);
   _effect.run();
   return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -28,4 +28,26 @@ describe("effect", () => {
     expect(foo).toBe(12);
     expect(res).toBe("foo");
   });
+
+  it("execute schedule when updating", () => {
+    let dummy;
+    let run;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler },
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    expect(dummy).toBe(1);
+    run();
+    expect(dummy).toBe(2);
+  });
 });
