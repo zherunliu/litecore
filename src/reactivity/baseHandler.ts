b772ba4d6438a49dbee4623747e06bf82ffb93ea
zherunliu <1437257281@qq.com>
2026-02-06 19:51:04 +0800

    feat: readonly & refactor

---
+++ src/reactivity/baseHandler.ts
@@ -0,0 +1,41 @@
+import { track, trigger } from "./effect";
+
+/* Singleton */
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    // 依赖收集
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  };
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    // 触发更新
+    trigger(target, key);
+    return res;
+  };
+}
+
+export const mutableHandler = {
+  get,
+  set,
+};
+
+export const readonlyHandler = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(
+      `Failed to set value: ${value} for key: ${key}, because target: ${target} is readonly.`,
+    );
+    return true;
+  },
+};
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,18 +1,13 @@
-import { track, trigger } from "./effect";
+import { mutableHandler, readonlyHandler } from "./baseHandler";
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      // 依赖收集
-      track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      // 触发更新
-      trigger(target, key);
-      return res;
-    },
-  });
+  return createActiveObject(raw, mutableHandler);
+}
+
+export function readonly(raw) {
+  return createActiveObject(raw, readonlyHandler);
+}
+
+function createActiveObject(raw: any, baseHandler) {
+  return new Proxy(raw, baseHandler);
 }
