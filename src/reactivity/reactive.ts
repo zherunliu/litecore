3de9ca8b9d5a2122dde7d87dd077f9dd14283acb
zherunliu <1437257281@qq.com>
2026-02-06 20:07:37 +0800

    feat: isReactive & isReadonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,11 +1,24 @@
 import { mutableHandler, readonlyHandler } from "./baseHandler";
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+}
+
 export function reactive(raw) {
   return createActiveObject(raw, mutableHandler);
 }
 
 export function readonly(raw) {
   return createActiveObject(raw, readonlyHandler);
+}
+
+export function isReactive(value) {
+  return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY];
 }
 
 function createActiveObject(raw: any, baseHandler) {
