25aeee663bcbfb1a8321b1a6d0ab98e9c9ae983b
zherunliu <1437257281@qq.com>
2026-02-17 22:48:28 +0800

    fix: fix import path

--- examples/componentEmit/main.js
+++ examples/componentEmit/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "../../lib/lite-core.esm.js";
-import { App } from "../componentEmit/App.js";
+import { App } from "./App.js";
 
 const rootComponent = document.querySelector("#app");
 createApp(App).mount(rootComponent);
--- examples/currentInstance/main.js
+++ examples/currentInstance/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "../../lib/lite-core.esm.js";
-import { App } from "../currentInstance/App.js";
+import { App } from "./App.js";
 
 const rootComponent = document.querySelector("#app");
 createApp(App).mount(rootComponent);
